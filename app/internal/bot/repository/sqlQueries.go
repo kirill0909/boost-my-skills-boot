8036276569fd4cea69c493d59105914833f296ae
kir1ll0909 <kir1ll0909@axarea.ru>
2023-08-27 11:46:37 +0300

    fix get uuid logic

--- app/internal/bot/repository/sqlQueries.go
+++ app/internal/bot/repository/sqlQueries.go
@@ -11,7 +11,7 @@ const (
      WHEN u.name = $1 AND tg_chat_id = $2 THEN TRUE
      ELSE FALSE
  END AS result
- FROM users.user u
+ FROM users.user u WHERE u.id = 1
   `
 
 	queryUserActivation = `

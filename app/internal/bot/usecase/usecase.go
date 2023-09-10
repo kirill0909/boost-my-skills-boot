9833fb4b331612870927885170e2048b96337e8a
kir1ll0909 <kir1ll0909@axarea.ru>
2023-09-10 16:17:37 +0300

    move add info callbacks to separate file

--- app/internal/bot/usecase/usecase.go
+++ app/internal/bot/usecase/usecase.go
@@ -116,81 +116,6 @@ func (u *BotUC) GetSubSubdirections(ctx context.Context, params models.GetSubSub
 	return u.pgRepo.GetSubSubdirections(ctx, params)
 }
 
-func (u *BotUC) HandleAskMeSubdirectionCallbackData(ctx context.Context, params models.AskMeParams) (err error) {
-	if err = u.hideKeyboard(params.ChatID, params.MessageID); err != nil {
-		return
-	}
-
-	splitedCallbackData := strings.Split(params.CallbackData, " ")
-	subdirectionIDCallbackData := splitedCallbackData[len(splitedCallbackData)-2]
-
-	subdirectionID, err := strconv.Atoi(subdirectionIDCallbackData)
-	if err != nil {
-		err = errors.Wrapf(err, "BotUC.HandleAskMeSubdirectionCallbackData.Atoi(%s)", subdirectionIDCallbackData)
-		return
-	}
-	params.SubdirectionID = subdirectionID
-
-	subSubdirections := u.stateDirections.GetSubSubdirectionsBySubdirectionID(params.SubdirectionID)
-
-	n := len(subSubdirections)
-	switch {
-	case n > 0:
-		if err = u.handleAskMeSubSubdirectionsCase(ctx, params); err != nil {
-			return
-		}
-	default:
-		if err = u.handleAskMeSubdirectionsDefaultCase(ctx, params); err != nil {
-			return
-		}
-	}
-
-	return
-}
-
-func (u *BotUC) handleAskMeSubSubdirectionsCase(ctx context.Context, params models.AskMeParams) (err error) {
-
-	subSubdirections := u.stateDirections.GetSubSubdirectionsBySubdirectionID(params.SubdirectionID)
-	u.stateUsers[params.ChatID] = models.AddInfoParams{
-		SubdirectionID: params.SubdirectionID,
-	}
-
-	msg := tgbotapi.NewMessage(params.ChatID, subSubdirectionAskMeMessage)
-	msg.ReplyMarkup = u.createSubSubdirectionsKeyboardAskMe(subSubdirections)
-	if _, err = u.BotAPI.Send(msg); err != nil {
-		return errors.Wrap(err, "BotUC.handleAskMeSubSubdirectionsCase.Send()")
-	}
-
-	return
-}
-
-func (u *BotUC) handleAskMeSubdirectionsDefaultCase(ctx context.Context, params models.AskMeParams) (err error) {
-
-	result, err := u.pgRepo.GetRandomQuestion(ctx, models.AksMeCallbackParams{
-		ChatID:         params.ChatID,
-		SubdirectionID: params.SubdirectionID})
-	if err != nil {
-		return
-	}
-
-	if len(result.Question) == 0 {
-		msg := tgbotapi.NewMessage(params.ChatID, notQuestionsMessage)
-		if _, err = u.BotAPI.Send(msg); err != nil {
-			return errors.Wrap(err, "BotUC.hanleAskMeSubdirectionsDefaultCase.len(result.Question).Send")
-		}
-
-		return
-	}
-
-	msg := tgbotapi.NewMessage(params.ChatID, result.Question)
-	msg.ReplyMarkup = u.createAnswerKeyboard(result.QuestionID)
-	if _, err = u.BotAPI.Send(msg); err != nil {
-		return errors.Wrap(err, "BotUC.hanleAskMeSubdirectionsDefaultCase.Send")
-	}
-
-	return
-}
-
 func (u *BotUC) HandleAskMeCommand(ctx context.Context, params models.AskMeParams) (err error) {
 	directionID, err := u.pgRepo.GetDirectionIDByChatID(ctx, params.ChatID)
 	if err != nil {
@@ -203,49 +128,6 @@ func (u *BotUC) HandleAskMeCommand(ctx context.Context, params models.AskMeParam
 	msg.ReplyMarkup = u.createSubdirectionsKeyboardAskMe(subdirections)
 	if _, err = u.BotAPI.Send(msg); err != nil {
 		return errors.Wrap(err, "BotUC.HandleAskMeCommand.Send")
-	}
-
-	return
-}
-
-func (u *BotUC) HandleAskMeSubSubdirectionCallbackData(ctx context.Context, params models.AskMeParams) (err error) {
-	if err = u.hideKeyboard(params.ChatID, params.MessageID); err != nil {
-		return
-	}
-
-	splitedCallbackData := strings.Split(params.CallbackData, " ")
-	subSubdirectionIDCallbackData := splitedCallbackData[len(splitedCallbackData)-2]
-
-	subSubdirectionID, err := strconv.Atoi(subSubdirectionIDCallbackData)
-	if err != nil {
-		err = errors.Wrapf(err, "BotUC.HandleAskMeSubdirectionCallbackData.Atoi(%s)", subSubdirectionIDCallbackData)
-		return
-	}
-	params.SubSubdirectionID = subSubdirectionID
-	params.SubdirectionID = u.stateUsers[params.ChatID].SubdirectionID
-
-	result, err := u.pgRepo.GetRandomQuestion(ctx, models.AksMeCallbackParams{
-		ChatID:            params.ChatID,
-		SubdirectionID:    params.SubdirectionID,
-		SubSubdirectionID: params.SubSubdirectionID,
-	})
-	if err != nil {
-		return
-	}
-
-	if len(result.Question) == 0 {
-		msg := tgbotapi.NewMessage(params.ChatID, notQuestionsMessage)
-		if _, err = u.BotAPI.Send(msg); err != nil {
-			return errors.Wrap(err, "BotUC.HandleAskMeSubSubdirectionCallbackData.len(result.Question).Send")
-		}
-
-		return
-	}
-
-	msg := tgbotapi.NewMessage(params.ChatID, result.Question)
-	msg.ReplyMarkup = u.createAnswerKeyboard(result.QuestionID)
-	if _, err = u.BotAPI.Send(msg); err != nil {
-		return errors.Wrap(err, "BotUC.HandleAskMeSubSubdirectionCallbackData.Send")
 	}
 
 	return

2f809398f4f07ace0d9ac50c128c7837eca5bcaa
kir1ll0909 <kir1ll0909@axarea.ru>
2023-09-10 16:14:35 +0300

    move add info commands to separate file

--- app/internal/bot/usecase/usecase.go
+++ app/internal/bot/usecase/usecase.go
@@ -116,40 +116,6 @@ func (u *BotUC) GetSubSubdirections(ctx context.Context, params models.GetSubSub
 	return u.pgRepo.GetSubSubdirections(ctx, params)
 }
 
-func (u *BotUC) HandleAddInfoCommand(ctx context.Context, chatID int64) (err error) {
-
-	u.stateUsers[chatID] = models.AddInfoParams{State: awaitingSubdirection}
-	directionID, err := u.pgRepo.GetDirectionIDByChatID(ctx, chatID)
-	if err != nil {
-		return
-	}
-
-	subdirections := u.stateDirections.GetSubdirectionsByDirectionID(directionID)
-	if len(subdirections) == 0 {
-		err = fmt.Errorf("subdirections not found")
-		return errors.Wrap(err, "BotUC.HandleAddInfoCommand.len(subdirections)")
-	}
-
-	msg := tgbotapi.NewMessage(chatID, "")
-	if len(subdirections) == 0 {
-		msg.Text = noOneSubdirectionsFoundMessage
-		if _, err = u.BotAPI.Send(msg); err != nil {
-			return
-		}
-		u.stateUsers[chatID] = models.AddInfoParams{State: idle}
-
-		return
-	}
-
-	msg.Text = subdirectionAddInfoMessage
-	msg.ReplyMarkup = u.createSubdirectionsKeyboardAddInfo(subdirections)
-	if _, err = u.BotAPI.Send(msg); err != nil {
-		return
-	}
-
-	return
-}
-
 func (u *BotUC) HandleAskMeSubdirectionCallbackData(ctx context.Context, params models.AskMeParams) (err error) {
 	if err = u.hideKeyboard(params.ChatID, params.MessageID); err != nil {
 		return

44cde9350b1c8a48839c311e3fc9f249bdbbdbf4
kir1ll0909 <kir1ll0909@axarea.ru>
2024-05-19 12:06:31 +0300

    add regex for message formating

--- app/pkg/utils/utils.go
+++ app/pkg/utils/utils.go
@@ -1,53 +1,22 @@
 package utils
 
 import (
-	"strings"
+	"regexp"
 )
 
 func FormatSnipets(message string) string {
-	switch {
-	case strings.Contains(message, SnipetGoBegin):
-		message = strings.Replace(message, SnipetGoBegin, "```go\n", -1)
-		message = strings.Replace(message, SnipetEnd, "```\n", -1)
-	case strings.Contains(message, SnipetBashBegin):
-		message = strings.Replace(message, SnipetBashBegin, "```bash\n", -1)
-		message = strings.Replace(message, SnipetEnd, "```\n", -1)
-	case strings.Contains(message, SnipetSQLBegin):
-		message = strings.Replace(message, SnipetSQLBegin, "```sql\n", -1)
-		message = strings.Replace(message, SnipetEnd, "```\n", -1)
-	case strings.Contains(message, SnipetRustBegin):
-		message = strings.Replace(message, SnipetRustBegin, "```rust\n", -1)
-		message = strings.Replace(message, SnipetEnd, "```\n", -1)
-	}
+	reBegin := regexp.MustCompile("snipet (go|bash|sql|rust|proto) begin")
+	reEnd := regexp.MustCompile("snipet end")
+
+	message = reBegin.ReplaceAllString(message, "```$1")
+	message = reEnd.ReplaceAllString(message, "```")
 
 	return message
 }
 
 func FormatBadCharacters(text string) string {
-	text = strings.ReplaceAll(text, "{", "\\{")
-	text = strings.ReplaceAll(text, "}", "\\}")
-	text = strings.ReplaceAll(text, "(", "\\(")
-	text = strings.ReplaceAll(text, ")", "\\)")
-	text = strings.ReplaceAll(text, "[", "\\[")
-	text = strings.ReplaceAll(text, "]", "\\]")
-	text = strings.ReplaceAll(text, "*", "\\*")
-	text = strings.ReplaceAll(text, "%", "\\%")
-	text = strings.ReplaceAll(text, "$", "\\$")
-	text = strings.ReplaceAll(text, "&", "\\&")
-	text = strings.ReplaceAll(text, "#", "\\#")
-	text = strings.ReplaceAll(text, ".", "\\.")
-	text = strings.ReplaceAll(text, ">", "\\>")
-	text = strings.ReplaceAll(text, "<", "\\<")
-	text = strings.ReplaceAll(text, "?", "\\?")
-	text = strings.ReplaceAll(text, "!", "\\!")
-	text = strings.ReplaceAll(text, "+", "\\+")
-	text = strings.ReplaceAll(text, "-", "\\-")
-	text = strings.ReplaceAll(text, "_", "\\_")
-	text = strings.ReplaceAll(text, ";", "\\;")
-	text = strings.ReplaceAll(text, "/", "\\/")
-	text = strings.ReplaceAll(text, "@", "\\@")
-	text = strings.ReplaceAll(text, "=", "\\=")
-	text = strings.ReplaceAll(text, "|", "\\|")
+	re := regexp.MustCompile(`([{}()\[\]*%$&#!.<>\?+\-_;/@=|])`)
+	text = re.ReplaceAllString(text, "\\$1")
 
 	return text
 }

079102faef0290893226d6a33251ed72696737ea
kir1ll0909 <kir1ll0909@axarea.ru>
2024-06-01 13:07:14 +0300

    fix sql query

--- app/internal/statistics/repository/sqlQueries.go
+++ app/internal/statistics/repository/sqlQueries.go
@@ -2,6 +2,6 @@ package repository
 
 const (
 	queryGetStatistics = `
-	SELECT COUNT(1) FROM infos WHERE EXTRACT(EPOCH from created_at) between $1 AND $2;
+	SELECT COUNT(1) FROM infos WHERE EXTRACT(EPOCH FROM created_at) between $1 AND $2;
 	`
 )
